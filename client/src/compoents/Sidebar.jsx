b787612bb7031667ee39ffa31a2ce7cac1442e92
Kishan Ahir <kk57161@gmail.com>
2026-02-20 17:21:49 +0530

    bookmark

--- client/src/compoents/Sidebar.jsx
+++ client/src/compoents/Sidebar.jsx
@@ -4,10 +4,9 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <>
-      <aside className="sm:w-64 min-h-screen text-white bg-blue-700 fixed ">
+      <aside className="w-64 min-h-screen text-white bg-blue-700 fixed ">
         <div className="p-4 text-2xl font-bold border-b border-blue-500">
           <b>Campus Notes</b>
-
         </div>
         <nav className="flex mt-6 flex-col gap-2 px-4">
           <Link to="/dashboard" className="hover:bg-blue-600 p-2 rounded">
